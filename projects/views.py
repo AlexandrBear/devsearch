5f4aa4b7ae1ca1aa10f89271c6ed6a9b0a2d2e20
Alexandr <sasha@gmail.ru>
2022-05-18 00:44:28 +0300

    Расширяет views

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website.'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'This was a project where i built put my portfolio.'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'Awesome open source project i am still working on.'
+    },
+]
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'projects'
+    number = 10
+    context = {'page':page, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
