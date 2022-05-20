a48d55ade7cb9cbd08f7d8c83ef4fe23cb163594
Alexandr <sasha@gmail.ru>
2022-05-20 21:42:59 +0300

    Add theme.

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,6 @@
 from unittest import mock
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import Project
 
 
@@ -8,3 +9,18 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'featured_image', 'description',
                   'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+        
+        # self.fields['title'].widget.attrs.update(
+        #     {'class': 'input'})
+
+        # self.fields['description'].widget.attrs.update(
+        #     {'class': 'input'})
