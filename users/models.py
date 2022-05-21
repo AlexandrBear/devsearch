6899ef381a7a4399d1250bbbe2eed88edbed4ae8
Alexandr <sasha@gmail.ru>
2022-05-21 14:48:39 +0300

    Add signals post_save and post_delete Profile

--- users/models.py
+++ users/models.py
@@ -25,7 +25,7 @@ class Profile(models.Model):
                           primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
