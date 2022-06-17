bfa989c446acd7b18f5d234bf9424551da50bb41
Alexandr <sasha@gmail.ru>
2022-06-17 12:39:05 +0300

    Adds sending Welcome Emails

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Profile
 
 
@@ -14,6 +16,17 @@ def createProfile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad are here!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 
 
