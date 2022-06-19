93faf5ed5d61dc4ef950c92e03706ab9e725ca86
Alexandr <sasha@gmail.ru>
2022-06-19 12:24:40 +0300

    Adds Authentication with JSON web token

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .serializers import ProjectSerializer
